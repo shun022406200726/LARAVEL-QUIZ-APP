291f41fa20fd7a2949d9fd6c14b36cce6c40f053
shun022406200726 <shun.085@gmail.com>
2024-01-24 14:40:11 +0900

    マイグレーション設定

---
+++ database/migrations/2024_01_24_021355_create_quisses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuissesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('question',255);
+            $table->string('answer_a',255);
+            $table->string('answer-b',255);
+            $table->string('answer-c',255);
+            $table->string('answer-d',255);
+            $table->enum('correct_answer',["A","B","C","D"]);
+            $table->text('explanation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
